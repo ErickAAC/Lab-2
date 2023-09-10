db30869fbfc1d4661cfaa7f4c29007ad3925f094
ErickAAC <erick.altamirano@yachaytech.edu.ec>
2023-09-10 11:38:24 -0500

    first commit

---
+++ Lab2/E.py
@@ -0,0 +1,36 @@
+from Crypto.Cipher import AES
+import binascii
+import hashlib
+import sys
+import Padding
+
+def decrypt_aes_ecb(ciphertext, key):
+    try:
+        # Ensure the key length is 32 bytes (256 bits)
+        if len(key) != 32:
+            raise ValueError("Key must be 32 bytes (256 bits) long")
+
+        # Convert the ciphertext from hex to bytes
+        ciphertext_bytes = binascii.unhexlify(ciphertext)
+
+        # Initialize the AES cipher in ECB mode with the provided key
+        cipher = AES.new(key, AES.MODE_ECB)
+
+        # Decrypt the ciphertext
+        decrypted_data = cipher.decrypt(ciphertext_bytes)
+
+        return decrypted_data
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    ciphertext = "b436bd84d16db330359edebf49725c62"
+    password = "hello"
+
+    # Derive the 256-bit AES key from the password using PBKDF2
+    salt = b'SomeSaltHere'  # You can change the salt value
+    key = key = hashlib.sha256(password.encode()).digest()
+
+    decrypted_data = decrypt_aes_ecb(ciphertext, key)
+    plaintext = Padding.removePadding(decrypted_data.decode(), mode='CMS')
+    print("  decrypt: ", plaintext)
